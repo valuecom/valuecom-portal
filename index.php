0205e30bf647a423b2c166a88096dd3e39341f63
valuecom <devs@valuecom.gr>
2024-10-17 15:47:10 +0300

    "Added link to output.css stylesheet and a div element to the body"

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="output.css" rel="stylesheet">
 </head>
 <body>
-    
+    <div class="w-full z-10 flex visible h-auto"></div>
 </body>
 </html>
